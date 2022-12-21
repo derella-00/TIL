a41ff8299b63b05149c4c75f32049904fd2cef4e
(derella_00) <(00.derella@gmail.com)>
2022-12-21 21:41:23 +0900

    algorithms

---
+++ 07_algorithms/progrmmers/옹알이/sol.py
@@ -0,0 +1,12 @@
+def solution(babbling):
+    answer = 0
+    return answer
+
+ex = [
+    ["aya", "yee", "u", "maa", "wyeoo"],
+    ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
+]
+
+for tc in range(2):
+    print(solution(tc))
+
